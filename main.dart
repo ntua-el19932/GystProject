a371747c4b2e172c53aee250a9231582cc704c88
ntua-el19932 <81427931+ntua-el19932@users.noreply.github.com>
2023-02-23 18:31:14 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -13,39 +13,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Gyst',
+      theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFFD3D3E2)),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Gyst'),
-        ),
-        body: const Center(
-          child: Text('Welcome')
-        ),
+        //appBar: AppBar(
+        //  title: const Text('Gyst'),
+        //),
+        body: const Center(child: Text('Welcome')),
         bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.deepPurple[700], 
+          backgroundColor: Color(0xFF5B5F97),
           selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.black45,
+          unselectedItemColor: Colors.black54,
           type: BottomNavigationBarType.fixed,
           items: const [
             BottomNavigationBarItem(
-               label: 'Home',
-               icon: Icon(Icons.home),
-              ),
-              BottomNavigationBarItem(
-               label: 'Menu',
-               icon: Icon(Icons.format_list_bulleted_rounded)
-              ),
-              BottomNavigationBarItem(
-               label: 'Add',
-               icon: Icon(Icons.add),
-              ),
-              BottomNavigationBarItem(
-               label: 'Focus',
-               icon: Icon(Icons.brightness_2_outlined)
-              ),
-              BottomNavigationBarItem(
-               label: 'Profile',
-               icon: Icon(Icons.person_outlined)
-              )
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+                label: 'Menu', icon: Icon(Icons.format_list_bulleted_rounded)),
+            BottomNavigationBarItem(
+              label: 'Add',
+              icon: Icon(Icons.add),
+            ),
+            BottomNavigationBarItem(
+                label: 'Focus', icon: Icon(Icons.brightness_2_outlined)),
+            BottomNavigationBarItem(
+                label: 'Profile', icon: Icon(Icons.person_outlined))
           ],
         ),
       ),
