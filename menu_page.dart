09bd31991bc681a89d998e5f4a69ae9715109df0
ntua-el19613 <115019595+ntua-el19613@users.noreply.github.com>
2023-02-24 22:22:27 +0200

    Add files via upload

--- menu_page.dart
+++ menu_page.dart
@@ -1,10 +1,252 @@
 import 'package:flutter/material.dart';
+import 'package:gyst/monthly_planner.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Menu'),);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MENU",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0)),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF8787C1),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Center(
+                child: Wrap(
+                  spacing: 20.0,
+                  runSpacing: 20.0,
+                  children: [
+                    SizedBox(
+                      width: 160.0,
+                      height: 100.0,
+                      child: Card(
+                        color: const Color(0xFFB7B7D4),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: const [
+                                SizedBox(height: 10.0),
+                                Text(
+                                  "Study Planner",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    height: 2.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 160.0,
+                      height: 100.0,
+                      child: Card(
+                        color: const Color(0xFFB7B7D4),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: const [
+                                SizedBox(height: 10.0),
+                                Text(
+                                  "Month Planner",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    height: 2.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 160.0,
+                      height: 100.0,
+                      child: Card(
+                        color: const Color(0xFFB7B7D4),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: const [
+                                SizedBox(height: 10.0),
+                                Text(
+                                  "Shopping List",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    height: 2.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 160.0,
+                      height: 100.0,
+                      child: Card(
+                        color: const Color(0xFFB7B7D4),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: const [
+                                SizedBox(height: 10.0),
+                                Text(
+                                  "Workout",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    height: 2.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 160.0,
+                      height: 100.0,
+                      child: Card(
+                        color: const Color(0xFFB7B7D4),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: const [
+                                SizedBox(height: 10.0),
+                                Text(
+                                  "To do List",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    height: 2.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 160.0,
+                      height: 100.0,
+                      child: Card(
+                        color: const Color(0xFFB7B7D4),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: const [
+                                SizedBox(height: 10.0),
+                                Text(
+                                  "Watch List",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    height: 2.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 160.0,
+                      height: 100.0,
+                      child: Card(
+                        color: const Color(0xFFB7B7D4),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: const [
+                                SizedBox(height: 10.0),
+                                Text(
+                                  "Quick notes",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    height: 2.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+//  child: Center(
+//   child: ElevatedButton(
+//     onPressed: () {
+//       Navigator.of(context).push(
+//         MaterialPageRoute(builder: (BuildContext context) {
+//           return const MonthlyPlannerPage();
+//         })
+//       );
+//     },
