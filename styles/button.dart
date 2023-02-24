0f2d67cb7915e0cc1d7e136621e6318bcfb7fd13
ntua-el19932 <81427931+ntua-el19932@users.noreply.github.com>
2023-02-25 00:42:44 +0200

    Update button.dart

--- styles/button.dart
+++ styles/button.dart
@@ -2,10 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:startup_namer/styles/colors.dart';
 
 final ButtonStyle buttonPr = ElevatedButton.styleFrom(
+    textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
     minimumSize: Size(350, 80),
-    backgroundColor: button,
+    primary: button, //background color
+    onPrimary: black, //text color
     elevation: 0,
     shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(
       Radius.circular(10),
+    )));
+
+final ButtonStyle SaveButton = ElevatedButton.styleFrom(
+    textStyle: TextStyle(fontSize: 21),
+    minimumSize: Size(105, 38),
+    primary: dark, //background color
+    onPrimary: black, //text color
+    elevation: 0,
+    shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+      Radius.circular(100),
     )));
