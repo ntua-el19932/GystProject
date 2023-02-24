2e95d8c3896f0e039204bd20438bc614024a0601
ntua-el19932 <81427931+ntua-el19932@users.noreply.github.com>
2023-02-24 13:04:03 +0200

    Add files via upload

---
+++ styles/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color dark = Color(0xFF5B5F97);
+Color light = Color(0xFFD3D3E2);
+Color button = Color(0xFFC8C8DB);
+Color black = Color.fromARGB(255, 0, 0, 0);
