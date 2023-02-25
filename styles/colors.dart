93bf0cec5f095d31ce5fdab9d57e18fd685cc837
ntua-el19932 <81427931+ntua-el19932@users.noreply.github.com>
2023-02-25 18:19:30 +0200

    Update colors.dart

--- styles/colors.dart
+++ styles/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-Color dark = Color(0xFF5B5F97);
-Color light = Color(0xFFD3D3E2);
-Color button = Color(0xFFC8C8DB);
-Color black = Color.fromARGB(255, 0, 0, 0);
-Color grey = Color(0xFFA7A7A7);
-Color white = Color(0xFFFFFFFF);
+const Color dark = Color(0xFF5B5F97);
+const Color light = Color(0xFFD3D3E2);
+const Color button = Color(0xFFC8C8DB);
+const Color black = Color.fromARGB(255, 0, 0, 0);
+const Color grey = Color(0xFFA7A7A7);
+const Color white = Color(0xFFFFFFFF);
