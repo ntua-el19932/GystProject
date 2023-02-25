59aa4119d04a48012caf365a320387bd13f9d4f8
ntua-el19932 <81427931+ntua-el19932@users.noreply.github.com>
2023-02-25 21:11:46 +0200

    Update todo.dart

--- templates/todo.dart
+++ templates/todo.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gyst/styles/colors.dart';
+import 'package:gyst/widgets/todo_item.dart';
+import 'package:gyst/widgets/to_do.dart';
 
 class ToDoListPage extends StatefulWidget {
   const ToDoListPage({Key? key}) : super(key: key);
@@ -8,13 +11,15 @@ class ToDoListPage extends StatefulWidget {
 }
 
 class _ToDoListPageState extends State<ToDoListPage> {
+  final todolist = ToDo.todoList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("To Do List",
             style: TextStyle(
-                color: Colors.black,
+              color: Colors.black,
             )),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
@@ -24,9 +29,33 @@ class _ToDoListPageState extends State<ToDoListPage> {
         centerTitle: true,
         backgroundColor: const Color(0xFF8787C1),
       ),
-      body : const Center(
-        child: Text('New Page'),
-      ), 
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            const SizedBox(height: 20),
+            Column(
+              children: [
+                for (ToDo todo1 in todolist)
+                  ToDoItem(
+                    todo: todo1,
+                  ),
+              ],
+            ),
+            FloatingActionButton.extended(
+              onPressed: () {
+                // Navigator.push(context,MaterialPageRoute(builder: (context) => const AddnewTesk));
+              },
+              backgroundColor: dark,
+              icon: const Icon(
+                Icons.add,
+                color: white,
+              ),
+              label: const Text('Add New Task'),
+            )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
