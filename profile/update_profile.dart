2fc4015c81114755c60f3e53b68a6a8e0abf0d73
ntua-el19932 <81427931+ntua-el19932@users.noreply.github.com>
2023-02-26 19:45:30 +0200

    Update update_profile.dart

--- profile/update_profile.dart
+++ profile/update_profile.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gyst/constants.dart';
-import 'package:gyst/profile/profile_page.dart';
 import 'package:gyst/styles/button.dart';
 import 'package:gyst/styles/colors.dart';
 
@@ -15,20 +14,7 @@ class _UpdateProfileState extends State<UpdateProfile> {
   final emailController = TextEditingController();
   final nameController = TextEditingController();
 
-  String tempEmail = 'user1@gmail.com';
-  String tempName = 'user1';
   @override
-  void initState() {
-    super.initState();
-
-    emailController.addListener(() => setState(() {
-          userEmail = emailController.text;
-        }));
-    nameController.addListener(() => setState(() {
-          userName = nameController.text;
-        }));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,90 +28,90 @@ class _UpdateProfileState extends State<UpdateProfile> {
           },
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          const Text(
-            "Edit your profile",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-          Stack(
-            children: [
-              SizedBox(
-                width: 120,
-                height: 120,
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.asset(profileImage)),
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Container(
-                  width: 35,
-                  height: 35,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100), color: dark),
-                  child: IconButton(
-                    icon: const Icon(Icons.add_a_photo_outlined),
-                    color: Colors.black,
-                    iconSize: 20,
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ProfilePage()));
-                    },
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              "Edit your profile",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Stack(
+              children: [
+                SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(profileImage)),
                 ),
-              )
-            ],
-          ),
-          const SizedBox(height: 30),
-          TextFormField(
-            controller: nameController,
-            keyboardType: TextInputType.name,
-            textInputAction: TextInputAction.next,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF5B5F97))),
-              labelText: ("Name"),
-              hintText: "Maria",
-              fillColor: Colors.white,
-              focusColor: Color(0xFF5B5F97),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                    width: 35,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100), color: dark),
+                    child: IconButton(
+                      icon: const Icon(Icons.add_a_photo_outlined),
+                      color: Colors.black,
+                      iconSize: 20,
+                      onPressed: () {},
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-          const SizedBox(height: 10),
-          TextFormField(
-            //Email
-            controller: emailController,
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.done,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF5B5F97))),
-              labelText: ('Email'),
-              hintText: 'name@example.com',
-              prefixIcon: Icon(Icons.email, color: Color(0xFF5B5F97)),
-              hoverColor: Color(0xFF5B5F97),
-              focusColor: Color(0xFF5B5F97),
-              fillColor: Colors.white,
+            const SizedBox(height: 30),
+            TextFormField(
+              controller: nameController,
+              keyboardType: TextInputType.name,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5B5F97))),
+                labelText: ("Name"),
+                hintText: "Maria",
+                fillColor: Colors.white,
+                focusColor: Color(0xFF5B5F97),
+              ),
             ),
-            onFieldSubmitted: (value) => tempEmail = value,
-          ),
-          const SizedBox(height: 50),
-          ElevatedButton(
-            onPressed: () {
-              userEmail = tempEmail;
-              userName = tempName;
-              Navigator.of(context).pop();
-            },
-            style: saveButton,
-            child: const Text('Save', style: TextStyle(color: black)),
-          ),
-        ],
+            const SizedBox(height: 10),
+            TextFormField(
+              //Email
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5B5F97))),
+                labelText: ('Email'),
+                hintText: 'name@example.com',
+                prefixIcon: Icon(Icons.email, color: Color(0xFF5B5F97)),
+                hoverColor: Color(0xFF5B5F97),
+                focusColor: Color(0xFF5B5F97),
+                fillColor: Colors.white,
+              ),
+              //onFieldSubmitted: (value) => nameController.text = value,
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                userEmail = emailController.text;
+                userName = nameController.text;
+
+                emailController.clear();
+                nameController.clear();
+                Navigator.of(context).pop();
+              },
+              style: saveButton,
+              child: const Text('Save', style: TextStyle(color: black)),
+            ),
+          ],
+        ),
       ),
     );
   }
