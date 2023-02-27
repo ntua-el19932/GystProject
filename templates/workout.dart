2c137bfb8bdc93ea812c781f11b8c91770ce29da
ntua-el19932 <81427931+ntua-el19932@users.noreply.github.com>
2023-02-27 21:44:41 +0200

    Update workout.dart

--- templates/workout.dart
+++ templates/workout.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gyst/styles/colors.dart';
+import 'package:gyst/styles/button.dart';
+import 'package:gyst/templates/workout/abs/abs_page.dart';
+import 'package:gyst/templates/workout/cardio/cardio_page.dart';
+import 'package:gyst/templates/workout/upperbody/upperbody_page.dart';
+import 'package:gyst/templates/workout/legs/legday_page.dart';
 
 class WorkoutPage extends StatefulWidget {
   const WorkoutPage({Key? key}) : super(key: key);
@@ -14,7 +20,7 @@ class _WorkoutPageState extends State<WorkoutPage> {
       appBar: AppBar(
         title: const Text("Workout",
             style: TextStyle(
-                color: Colors.black,
+              color: Colors.black,
             )),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
@@ -23,10 +29,71 @@ class _WorkoutPageState extends State<WorkoutPage> {
         ),
         centerTitle: true,
         backgroundColor: const Color(0xFF8787C1),
+        actions: const [
+          Icon(
+            Icons.fitness_center_rounded,
+            color: black,
+          ),
+          Icon(Icons.check_box_outline_blank, color: Color(0xFF8787C1))
+        ],
       ),
-      body : const Center(
-        child: Text('New Page'),
-      ), 
+      body: SingleChildScrollView(
+          child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              ElevatedButton(
+                  child: const Text('Leg Day', style: TextStyle(color: black)),
+                  style: buttonPr,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LegPage()));
+                  }),
+              const SizedBox(height: 10),
+              const Divider(),
+              ElevatedButton(
+                style: buttonPr,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const UpperPage()));
+                },
+                child: const Text('Upper Body Day',
+                    style: TextStyle(color: black)),
+              ),
+              const SizedBox(height: 10),
+              const Divider(),
+              ElevatedButton(
+                  child: const Text('Cardio', style: TextStyle(color: black)),
+                  style: buttonPr,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CardioPage()));
+                  }),
+              const SizedBox(height: 10),
+              const Divider(),
+              ElevatedButton(
+                  child: const Text('Abs', style: TextStyle(color: black)),
+                  style: buttonPr,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AbsPage()));
+                  }),
+              const SizedBox(height: 10),
+              const Divider(),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
